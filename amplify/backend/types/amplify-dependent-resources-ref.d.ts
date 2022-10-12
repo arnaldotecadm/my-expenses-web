386d07d92488685c936f86ab1d1e99c63afa2737
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2022-10-12 13:23:12 +0100

    A few tweaks for the new version

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "myexpenses": {
+        "myexpensesweb": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
